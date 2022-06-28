15b88de33e7f0a39c66a19969c555544cc5de5ff
NeveSGG <step_nev2@mail.ru>
2022-06-28 20:28:51 +0600

    regform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+
 
 function App() {
+
+  const [usernameReg, setUsernameReg] = useState('')
+  const [emailReg, setEmailReg] = useState('')
+  const [passwordReg, setPasswordReg] = useState('')
+
+  const [emailLogin, setEmailLogin] = useState('')
+  const [passwordLogin, setPasswordLogin] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Registration</h1>
+        <input className='inp'
+          type='text'
+          placeholder='Username'
+          onChange={(e) => {
+            setUsernameReg(e.target.value)
+          }}
+        />
+        <input className='inp'
+          placeholder='Email'
+          type='email' 
+          onChange={(e) => {
+            setEmailReg(e.target.value)
+          }}
+        />
+        <input className='inp'
+          placeholder='Password'
+          type='password' 
+          onChange={(e) => {
+            setPasswordReg(e.target.value)
+          }}
+        />
+        <div className='superButton'>
+          <button className="glow-on-hover">Sign up</button>
+        </div>
+      
+        <h1>Logging in</h1>
+        <input className='inp'
+          type='email'
+          placeholder='Email'
+          onChange={(e) => {
+            setEmailLogin(e.target.value)
+          }}
+        />
+        <input className='inp'
+          placeholder='Password'
+          type='password' 
+          onChange={(e) => {
+            setPasswordLogin(e.target.value)
+          }}
+        />
+        <div className='superButton'>
+          <button className="glow-on-hover">Sign in</button>
+        </div>
     </div>
   );
 }
